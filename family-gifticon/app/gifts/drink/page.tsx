da60ba09ac4d8277d91590aaa2fa1cfaaba0fcd6
Ho Nyeong <dev.honing@gmail.com>
2024-03-09 15:16:15 +0900

    Feat: 음료 페이지와 음식 페이지 라우트 추가

---
+++ family-gifticon/app/gifts/drink/page.tsx
@@ -0,0 +1,5 @@
+const DrinkPage = () => {
+  return <h1>음료 페이지</h1>;
+};
+
+export default DrinkPage;
---
+++ family-gifticon/app/gifts/food/page.tsx
@@ -0,0 +1,4 @@
+const FoodPage = () => {
+  return <h1>음식 페이지</h1>;
+};
+export default FoodPage;
