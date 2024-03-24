44a70956ed401faa047161421d31ec83d42c404c
Ho Nyeong <dev.honing@gmail.com>
2024-03-24 20:14:56 +0900

    Style: 기프티콘 페이지를 카테고리별 분류

--- family-gifticon/app/gifts/page.tsx
+++ family-gifticon/app/gifts/page.tsx
@@ -1,12 +1,22 @@
-"use client"
+"use client";
 // app/gifts/page.tsx
-import GiftLists from '../../components/GiftLists';
+import GiftLists from "../../components/GiftLists";
+import Link from "next/link";
 
 const GiftsPage = () => {
   return (
     <div>
       <h1>기프티콘 보기</h1>
-      <GiftLists />
+      <h2 style={{display: "flex", justifyContent: "center"}}>카테고리</h2>
+      <span style={{display: "flex", justifyContent: "space-around", }}>
+        <p style={{fontSize: "2rem", border: "1px solid #000"}}>
+          <Link href="/gifts/food">음식</Link>
+        </p>
+        <p style={{fontSize: "2rem", border: "1px solid #000"}}>
+          <Link href="/gifts/drink">음료</Link>
+        </p>
+      </span>
+      {/* <GiftLists /> */}
     </div>
   );
 };
