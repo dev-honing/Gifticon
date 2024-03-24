aa95c03df45d7f3f4b37c35635fc2bff72fa4125
Ho Nyeong <dev.honing@gmail.com>
2024-03-24 16:48:13 +0900

    Layout: header 레이아웃에 로그인/회원가입 링크 추가

--- family-gifticon/app/layout.tsx
+++ family-gifticon/app/layout.tsx
@@ -14,9 +14,29 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body>
-        <header style={{ backgroundColor: "lightblue", padding: "1rem" }}>
-          <span style={{fontSize: "2rem"}}>
-          <Link href="/">메인 페이지</Link>
+        <header
+          style={{
+            backgroundColor: "lightblue",
+            padding: "1rem",
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center"
+          }}
+        >
+          <span style={{ fontSize: "2rem" }}>
+            <Link href="/">메인 페이지</Link>
+          </span>
+          <span
+            style={{
+              backgroundColor: "#d9d9d9",
+              display: "flex",
+              alignItems: "center",
+              padding: "1rem",
+            }}
+          >
+            <Link href="/auth/login">로그인</Link>
+            <span style={{ margin: "0 1rem" }}>|</span>
+            <Link href="/auth/signup">회원가입</Link>
           </span>
         </header>
         {children}
