349421368f2c9181eff5220cd926bea3514c1aed
Ho Nyeong <dev.honing@gmail.com>
2024-04-04 21:02:59 +0900

    Feat: 파싱을 위한 미들웨어 설정을 추가하고, 이미지 업로드 구현

--- Storage/app.js
+++ Storage/app.js
@@ -8,6 +8,17 @@ const app = express();
 const dotenv = require("dotenv");
 const AWS = require("aws-sdk");
 
+// 미들웨어 패키지
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const multer = require("multer");
+
+// 미들웨어 설정
+const upload = multer(); // multer
+app.use(cors({ origin: "http://localhost:3000" })); // cors
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // 환경변수 로드
 dotenv.config({ path: "../.env" });
 
@@ -23,8 +34,30 @@ app.get("/", (req, res) => {
 });
 
 // 이미지 업로드 라우트
-app.get("/upload", (req, res) => { // fixme: POST 요청으로 변경
-  res.send("이미지 업로드 라우트 확인");
+app.post("/upload", upload.single('image'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: "이미지가 제공되지 않았습니다." });
+  }
+
+  const imageData = req.file.buffer; // multer로 파싱된 이미지 데이터
+
+  // S3에 업로드할 파일 설정
+  const params = {
+    Bucket: process.env.S3_BUCKET_NAME,
+    Key: "something.jpg", // 파일 이름은 고유해야!
+    Body: imageData,
+    ContentType: "image/jpeg", // 이미지 타입에 따라 변경
+  };
+
+  // S3에 파일 업로드
+  s3.upload(params, (err, data) => {
+    if (err) {
+      console.error("S3 업로드 중 오류:", err);
+      return res.status(500).json({ error: "이미지를 업로드하는 동안 오류가 발생했습니다." });
+    }
+    // 성공적으로 업로드된 경우 클라이언트에게 응답
+    res.json({ imageUrl: data.Location });
+  });
 });
 
 const PORT = 5555;
