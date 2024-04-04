cbc588716d80ad8b7926e7a2449bdf0338619591
Ho Nyeong <dev.honing@gmail.com>
2024-04-04 20:43:21 +0900

    Feat: 이미지 업로드 폼 작성

--- family-gifticon/app/upload/page.tsx
+++ family-gifticon/app/upload/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 // app/upload/page.tsx
+import ImageUploadForm from '../../components/ImageUploadForm';
 
 const UploadPage = () => {
   return (
     <>
       <h1>업로드 페이지</h1>
+      <ImageUploadForm />
     </>
   );
 };
