dd4598627890f569c7af835f6e0504ffee438ed8
Ho Nyeong <dev.honing@gmail.com>
2024-04-04 18:10:46 +0900

    Test: /upload 라우트 확인
    - 추후 POST 요청으로 변경

--- Storage/app.js
+++ Storage/app.js
@@ -7,6 +7,11 @@ app.get("/", (req, res) => {
   res.send("Express 서버 제작 확인");
 });
 
+// 이미지 업로드 라우트
+app.get("/upload", (req, res) => { // fixme: POST 요청으로 변경
+  res.send("이미지 업로드 라우트 확인");
+});
+
 const PORT = 5555;
 app.listen(PORT, () => {
   console.log(`서버 ON: http://localhost:${PORT}`);
