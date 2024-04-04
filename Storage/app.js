1003358a9a39b8fc5bce39aa86a39f0a00bdc926
Ho Nyeong <dev.honing@gmail.com>
2024-04-04 18:18:22 +0900

    Feat: S3 연결 설정 및 환경변수 로드

--- Storage/app.js
+++ Storage/app.js
@@ -1,7 +1,22 @@
 // Storage/app.js
 
+// Express 서버 제작을 위한 패키지
 const express = require("express");
 const app = express();
+
+// S3 연결 설정을 위한 패키지
+const dotenv = require("dotenv");
+const AWS = require("aws-sdk");
+
+// 환경변수 로드
+dotenv.config({ path: "../.env" });
+
+// AWS S3 설정
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+});
 
 app.get("/", (req, res) => {
   res.send("Express 서버 제작 확인");
